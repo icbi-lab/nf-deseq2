2905edf017f2fa07fb8c9393130fb3ac4f28e42b
ClaudiaSteinacher <94630988+ClaudiaSteinacher@users.noreply.github.com>
2022-01-27 00:17:29 +0100

    GO_TopGo-codeCleanup-fileAsCommandArg

--- modules/GO_TOPGO.R
+++ modules/GO_TOPGO.R
@@ -1,87 +1,39 @@
-### Load package and set path
+#Get input arguments
+arg = commandArgs(trailingOnly=TRUE)
+
+#Test if there is a inputfile argument
+if (length(args)!=1) {
+  stop("You have to enter the full path to your source file as parameter!", call.=FALSE)
+}
+
+#Load package and set path
 library("topGO")
-library("org.Mm.eg.db")
+library("org.Hs.eg.db")
 library("ggplot2")
 
-
-
-
-#### Load data
-
-## from file
-#data <- data.table::fread("RESULTS_DEG_HNSC.txt")
-#data$GeneID <- substr(data$GeneID, 1, 18)
-
-
-## from web-source
-data <- data.table::fread("https://raw.githubusercontent.com/ycl6/GO-Enrichment-Analysis-Demo/master/DESeq2_DEG.txt")
+#Load data from input file
+data <- data.table::fread(arg[1])
 data$GeneID <- substr(data$GeneID, 1, 18)
 
-
-## show raw-data for checking the load function
-data$GeneID
-
-
-
-
-### Define significance threshold
+#Define significance threshold
 up.idx <- which(data$padj < 0.05 & data$log2fc > 0) # FDR < 0.05 and logFC > 0
 dn.idx <- which(data$padj < 0.05 & data$log2fc < 0) # FDR < 0.05 and logFC < 0
 
-
-## show data dimension
-dim(data)
-
-
-## show length of up
-length(up.idx)
-
-
-## show length of down
-length(dn.idx)
-
-
-
-
-### Define significant genes
+#Define significant genes
 all.genes <- data$GeneSymbol
 up.genes <- data[up.idx,]$GeneSymbol
 dn.genes <- data[dn.idx,]$GeneSymbol
-
-
-## alternative if you have only Ensembl gene ID
+##Alternative if you have only Ensembl gene ID
 #all.genes <- data$GeneID
 #up.genes <- data[up.idx,]$GeneID
 #dn.genes <- data[dn.idx,]$GeneID
 
+#Decide the sub-ontology to test
+ontology <- "BP"  #(Biological Process)
+#ontology <- "CC" #(Cellular Component)
+#ontology <- "MF" #(Molecular Function)
 
-## show the first 10 gens of up
-head(up.genes, 10)
-
-
-## show the first 10 gens of dn
-head(dn.genes, 10)
-
-
-
-
-### Decide the sub-ontology to test
-
-## BP (Biological Process)
-ontology <- "BP"
-
-
-## CC (Cellular Component)
-#ontology <- "CC"
-
-
-## MF (Molecular Function)
-#ontology <- "MF"
-
-
-
-
-### Decide test algorithm
+#Decide test algorithm
 algorithm <- "weight01"
 #algorithm <- "classic"
 #algorithm <- "elim"
@@ -89,189 +41,88 @@ algorithm <- "weight01"
 #algorithm <- "lea"
 #algorithm <- "parentchild"
 
-
-
-
-### Define the statistical test used
-
-## for tests based on gene counts
+#Define the statistical test used
+#For tests based on gene counts
 statistic <- "fisher"      # Fischer's exact test
-
-
-## for tests based on gene scores or gene ranks
+#For tests based on gene scores or gene ranks
 #statistic <- "ks"         # Kolmogorov-Smirnov test
 #statistic <- "t"          # t-test
-
-
-## For tests based on gene expression
+#For tests based on gene expression
 #statistic <- "globaltest" # globaltest
 
-
-
-### Set outfile prefix
+#Set outfile prefix
 outTitle <- paste0("topGO_GO-", ontology, "_ORA_", algorithm,"_", statistic)
 
-## prints outline for controlling
-outTitle
-
-
-
-
-### Prepare input data
-
-## produce list of up's
+#Prepare input data
+#Produce list of up's
 upList <- factor(as.integer(all.genes %in% up.genes))
 names(upList) <- all.genes
-
-
-## show first 30 entrys
-head(upList, 30)
-
-
-## show ammount of entrys
-table(upList)
-
-
-## produce list of dn's
+#Produce list of dn's
 dnList <- factor(as.integer(all.genes %in% dn.genes))
 names(dnList) <- all.genes
 
-
-## show first 30 entrys
-head(dnList, 30)
-
-
-## show ammount of entrys
-table(dnList)
-
-
-
-
-### Create topGOdata object
-
-## of up
+#Create topGOdata object
+#Of up
 upGOdata <- new("topGOdata", ontology = ontology, allGenes = upList,geneSel = function(x)(x == 1), 
-                nodeSize = 10, annot = annFUN.org, mapping = "org.Mm.eg.db", ID = "SYMBOL")
-
-
-## of down 
+                nodeSize = 10, annot = annFUN.org, mapping = "org.Hs.eg.db", ID = "SYMBOL")
+#Of down 
 dnGOdata <- new("topGOdata", ontology = ontology, allGenes = dnList,geneSel = function(x)(x == 1), 
-                nodeSize = 10, annot = annFUN.org, mapping = "org.Mm.eg.db", ID = "SYMBOL")
+                nodeSize = 10, annot = annFUN.org, mapping = "org.Hs.eg.db", ID = "SYMBOL")
 
-
-
-
-### Test for enrichment
-
-## test up
+#Test for enrichment
+#Test up
 upRes <- runTest(upGOdata, algorithm = algorithm, statistic = statistic)
-
-
-## show up
-upRes
-
-
-## test dn
+#Test dn
 dnRes <- runTest(dnGOdata, algorithm = algorithm, statistic = statistic)
 
-
-## show dn
-dnRes
-
-
-
-
-### Plot enrichment
-
-## up-regulated genes
+#Plot enrichment
+#Up-regulated genes
 png(paste0(outTitle, "_up.png"), width = 8, height = 6, units = "in", res = 300)
 enrichment_barplot(upGOdata, upRes, showTerms = 20, numChar = 50, orderBy = "Scores", 
                    title = paste0("GO-", ontology," ORA of up-regulated genes"))
 invisible(dev.off())
-
-
-## down-regulated genes
+#Down-regulated genes
 png(paste0(outTitle, "_dn.png"), width = 8, height = 6, units = "in", res = 300)
 enrichment_barplot(dnGOdata, dnRes, showTerms = 20, numChar = 50, orderBy = "Scores",
                    title = paste0("GO-", ontology," ORA of down-regulated genes"))
 invisible(dev.off())
 
-
-
-
-### Create result table
-
-## build up result table
+#Create result table
+#Build up result table
 up.tab <- GenTable(upGOdata, Pval = upRes, topNodes = 20)
-
-
-## show up result table
-up.tab
-
-
-## build down result table 
+#Build down result table 
 dn.tab <- GenTable(dnGOdata, Pval = dnRes, topNodes = 20)
 
-
-## show down result table
-dn.tab
-
-
-
-
-### Update table with full GO term name
-
-## update up table 
+#Update table with full GO term name
+#Uupdate up table 
 up.tab$Term <- sapply(up.tab$"GO.ID", function(go) Term(GO.db::GOTERM[[go]]))
-
-
-## show up table
-up.tab$Term
-
-
-#update down table
+#Update down table
 dn.tab$Term <- sapply(dn.tab$"GO.ID", function(go) Term(GO.db::GOTERM[[go]]))
 
-
-## show down table
-dn.tab$Term
-
-
-
-
-### Add gene symbol to result table
-
-## Obtain the list of significant genes
+#Add gene symbol to result table to obtain the list of significant genes
 up.sigGenes <- sigGenes(upGOdata)
 dn.sigGenes <- sigGenes(dnGOdata)
 
-## Retrieve gene symbols for each GO from the test result
+#Retrieve gene symbols for each GO from the test result
 up.AnnoList <- lapply(up.tab$"GO.ID", 
                       function(x) as.character(unlist(genesInTerm(object = upGOdata, whichGO = x))))
 dn.AnnoList <- lapply(dn.tab$"GO.ID", 
                       function(x) as.character(unlist(genesInTerm(object = dnGOdata, whichGO = x))))
-
 up.SigList <- lapply(up.AnnoList, function(x) intersect(x, up.sigGenes))
 dn.SigList <- lapply(dn.AnnoList, function(x) intersect(x, dn.sigGenes))
 
-## Coerce gene list to a comma-separated vector
+#Coerce gene list to a comma-separated vector
 up.tab$Genes <- sapply(up.SigList, paste, collapse = ",")
 dn.tab$Genes <- sapply(dn.SigList, paste, collapse = ",")
 
-
-## cbind first 5 up 
+#Cbind first 5 up 
 cbind(head(up.tab$Genes, 5))
 
-
-## cbind first 5 down
+#Cbind first 5 down
 cbind(head(dn.tab$Genes, 5))
 
-
-
-
-### Output results to files
+#Output results to files
 write.table(up.tab, file = paste0(outTitle, "_up.txt"), sep = "\t", quote = F, 
             row.names = F, col.names = T)
-
 write.table(dn.tab, file = paste0(outTitle, "_dn.txt"), sep = "\t", quote = F, 
             row.names = F, col.names = T)
